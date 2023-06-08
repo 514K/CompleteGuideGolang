be74ccaa631ebed086dc7baaa7506bc7601e0459
514K <alekseysafronov0@gmail.com>
2023-06-08 18:00:53 +0300

    to home

---
+++ functionsTypes/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func myFunc1() {
+	fmt.Printf("1\n")
+}
+
+func myFunc2() {
+	fmt.Printf("2\n")
+}
+
+func myFunc3(mF func()) {
+	mF()
+}
+
+func myFunc4() func() {
+	if math.Mod(float64(rand.Int()), 10) > 5 {
+		return myFunc1
+	}
+	return myFunc2
+}
+
+type myFuncs func()
+
+func bettermyFunc3(mF myFuncs) {
+	mF()
+}
+
+func bettermyFunc4() myFuncs {
+	if math.Mod(float64(rand.Int()), 10) > 5 {
+		return myFunc1
+	}
+	return myFunc2
+}
+
+func retAnonFunc() myFuncs {
+	return func() {
+		fmt.Printf("Super anon func)\n")
+	}
+}
+
+var wtf = 1.23
+
+func currentFunc(val int) func() {
+	return func() {
+		fmt.Printf("%v\n", val)
+	}
+}
+
+func actualFunc(val *int) func() {
+	return func() {
+		fmt.Printf("%v\n", *val)
+	}
+}
+
+func main() {
+	fmt.Printf("Hello, funcs types\n")
+	var funcT func()
+	if math.Mod(float64(rand.Int()), 10) > 5 {
+		funcT = myFunc1
+	} else {
+		funcT = myFunc2
+	}
+
+	funcT()
+
+	myFunc3(funcT)
+
+	myFunc4()()
+
+	var funcTB myFuncs
+	if math.Mod(float64(rand.Int()), 10) > 5 {
+		funcTB = myFunc1
+	} else {
+		funcTB = myFunc2
+	}
+
+	bettermyFunc3(funcTB)
+	bettermyFunc4()()
+
+	anonFunc := func() {
+		fmt.Printf("Anon func ;)\n")
+	}
+	anonFunc()
+	retAnonFunc()()
+
+	staticVal := currentFunc(int(wtf))
+
+}
